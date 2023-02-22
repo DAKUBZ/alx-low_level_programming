89a7eb2244425983c770eaf24b45b8c899b11b02
DAKUBZ <tinekmarty@gmail.com>
2023-02-22 15:28:18 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - funtion that prints alphabet in lower cases
+ *
+ * Return: Always 0;
+ */
+
+
+void print_alphabet(void);
+
+{    
+	char i;
+
+	for (i = 'a' ; i < 'z' ; i++)
+	       _putchar(i);
+        _putchar('\n');	
+}	
