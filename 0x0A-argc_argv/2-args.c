4ad75dec2adbf25b292b30a25637d2155b6f4a67
DAKUBZ <tinekmarty@gmail.com>
2023-03-11 08:34:45 -0800

    3-mul.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include "main.h"
+
+
+
+
+
+
 /**
  * main - Entry Point
  * @argc: arguments
  * @argv: array pointing to arguments
  * Return: 0
  */
+
+
 int main(int argc, char *argv[])
-{
-	int i = 0;
+
+  int i = 0;
 
 	while (i < argc)
 	{
