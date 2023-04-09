3597e04dc8663462a06409f8fec1f0c056f850b7
DAKUBZ <tinekmarty@gmail.com>
2023-04-09 11:32:20 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,62 +1,35 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include "main.h"
-
 /**
- * append_text_to_file - apends text to file
- * @filename: path to file
- * @text_content: content
- * Return: 1 or -1
+ * append_text_to_file - append text to a file
+ *
+ * @filename: name of file to append to
+ * @text_content: content to be appended to file
+ *
+ * Return: 1 on success; -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t w;
-	int size;
+	FILE *file;
+	int success = 1;
 
-	if (!filename)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
-	if (fd == -1)
-		return (-1);
-
-	if (!text_content)
+	if (filename == NULL)
 	{
-		close(fd);
-		return (1);
-	}
-
-	size = _strlen(text_content);
-	w = write(fd, text_content, size);
-
-	if (w == -1)
-	{
-		close(fd);
 		return (-1);
 	}
-	close(fd);
-	return (1);
-}
-
-/**
- * _strlen - len
- *
- * @s: is a pointer to a char
- *
- * Return: Always 0.
- */
-
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (*(s + i) != '\0')
+	file = fopen(filename, "a");
+	if (file == NULL)
 	{
-		i++;
+		return (-1);
 	}
-
-	return (i);
+	if (text_content == NULL)
+	{
+		fclose(file);
+		return (success);
+	}
+	if (fprintf(file, "%s", text_content) < 0)
+	{
+		success = (-1);
+	}
+	fclose(file);
+	return (success);
 }
