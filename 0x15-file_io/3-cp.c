e84a465f4cc3cf61e529be190431b8e65ac5d1f9
DAKUBZ <tinekmarty@gmail.com>
2023-04-09 12:08:27 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,40 @@
 #include "main.h"
-
+#define BUFFLEN (1024)
 /**
- * get_char_count - get the number of characters to be read
- * @ac: argument count
- * @av: argument vector
- *
- * Return: number of characters
+ * main- function av y av
+ * @ac: file
+ * @av: number of letters of the file
+ * Return: numbers of letters or zero it fails
  */
-
-int get_char_count(int ac, char *av)
+int main(int ac, char *av[])
 {
+	int fd_from, fd_to, read_from, write_to, close_from, close_to;
+	char buffer[BUFFLEN];
+
 	if (ac != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fptr = fopen(av, "r");
-	if (fptr == NULL)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av);
-		exit(98);
-	}
-	for (c = getc(fptr); c != EOF; c = getc(fptr))
-		count += 1;
-	fclose(fptr);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	return (count);
-}
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-/**
- * main - entry point
- *
- * @argc: argument count: number of arguments supplied
- * @argv: argument vector: string or character arguments supplied
- *
- * Return: integer values
- */
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	while ((read_from = read(fd_from, buffer, BUFFLEN)) != '\0')
+	{
+		if (read_from == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		write_to = write(fd_to, buffer, read_from);
+		if (write_to == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
 
-int main(int argc, char *argv[])
-{
-	count = get_char_count(argc, argv[1]);
-
-	read_str = malloc(count);
-	open_val = open(argv[1], O_RDONLY);
-	read_val = read(open_val, read_str, count);
-	if ((open_val == -1) || (read_val == -1))
-	{
-		free(read_str);
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	to_open = open(argv[2], O_TRUNC | O_RDWR | O_CREAT, 0664);
-	write_val = write(to_open, read_str, read_val);
-	if ((to_open == -1) || (write_val == -1))
-	{
-		free(read_str);
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	close_to = close(to_open);
-	close_from = close(open_val);
-	if (close_to == -1)
-	{
-		free(read_str);
-		dprintf(2, "Error: Can't close fd %d\n", to_open);
-		exit(100);
-	}
+	close_from = close(fd_from);
 	if (close_from == -1)
-	{
-		free(read_str);
-		dprintf(2, "Error: Can't close fd %d\n", open_val);
-		exit(100);
-	} return (0);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_from), exit(100);
+	close_to = close(fd_to);
+		if (close_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_to), exit(100);
+	return (0);
 }
